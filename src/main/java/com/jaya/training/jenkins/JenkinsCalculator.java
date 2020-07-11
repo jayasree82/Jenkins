4e7e00458b1b6c725a2bd97f810a914da98c6632
jayasree82 <er.jayasree@yahoo.com>
2020-07-11 15:01:52 +0530

    Adding New files

---
+++ src/main/java/com/jaya/training/jenkins/JenkinsCalculator.java
@@ -0,0 +1,18 @@
+package com.jaya.training.jenkins;
+
+public class JenkinsCalculator {
+	//Method to add 2 Numbers
+	public int addNumbers(int numberOne,int numberTwo) {
+		return numberOne + numberTwo;
+		
+	}
+	
+		//Method to sub 2 Numbers
+	
+	public int subtractNumbers(int numberOne,int numberTwo) {
+		return numberOne - numberTwo;
+		
+	}
+
+
+}
---
+++ src/test/java/com/jaya/traning/jenkins/JenkinsCalcTest.java
@@ -0,0 +1,24 @@
+package com.jaya.traning.jenkins;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.jaya.training.jenkins.JenkinsCalculator;
+
+public class JenkinsCalcTest {
+
+	
+	@Test
+	public void addTest(){
+		JenkinsCalculator myCalc = new JenkinsCalculator();
+		assertEquals(10,myCalc.addNumbers(5,5));
+		}
+
+	@Test
+	public void subtractTest(){
+		JenkinsCalculator myCalc = new JenkinsCalculator();
+		assertEquals(5,myCalc.subtractNumbers(10,5));
+		}
+
+}
